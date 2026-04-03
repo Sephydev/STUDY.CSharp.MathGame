33355133ddb0039e765094c3e5421b9aacc4be29
Sephydev <nicolas.bussiere@outlook.fr>
2026-04-03 13:03:53 +0200

    Add Exit menu option

--- STUDY.CSharp.MathGame/Program.cs
+++ STUDY.CSharp.MathGame/Program.cs
@@ -1,18 +1,19 @@
 ﻿string? readInput;
 string mainMenuChoice = "";
 
-while (mainMenuChoice == "")
+while (!mainMenuChoice.Equals("exit"))
 {
     Console.Clear();
 
     Console.WriteLine("Please enter one of the following choices:");
     Console.WriteLine("  - New Game");
     Console.WriteLine("  - Game History");
+    Console.WriteLine("  - Exit");
 
     readInput = Console.ReadLine();
     if (readInput != "" && readInput != null)
     {
-        mainMenuChoice = readInput;
+        mainMenuChoice = readInput.ToLower();
         Console.WriteLine($"You've entered '{mainMenuChoice}'. Press Enter to return to the Main Menu.");
         Console.ReadLine();
     }
@@ -20,5 +21,7 @@ while (mainMenuChoice == "")
     {
         Console.WriteLine("Please enter a valid option. Press Enter to return to the Main Menu.");
         Console.ReadLine();
+        continue;
     }
+
 }
