9521bfd468f41b639dd7a51f5849031f3983c892
Sephydev <nicolas.bussiere@outlook.fr>
2026-04-03 12:54:06 +0200

    Take user input with basic validation

--- STUDY.CSharp.MathGame/Program.cs
+++ STUDY.CSharp.MathGame/Program.cs
@@ -1,3 +1,24 @@
-﻿Console.WriteLine("Please enter one of the following choices:");
-Console.WriteLine("  - New Game");
-Console.WriteLine("  - Game History");
+﻿string? readInput;
+string mainMenuChoice = "";
+
+while (mainMenuChoice == "")
+{
+    Console.Clear();
+
+    Console.WriteLine("Please enter one of the following choices:");
+    Console.WriteLine("  - New Game");
+    Console.WriteLine("  - Game History");
+
+    readInput = Console.ReadLine();
+    if (readInput != "" && readInput != null)
+    {
+        mainMenuChoice = readInput;
+        Console.WriteLine($"You've entered '{mainMenuChoice}'. Press Enter to return to the Main Menu.");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid option. Press Enter to return to the Main Menu.");
+        Console.ReadLine();
+    }
+}
